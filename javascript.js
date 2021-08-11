589a54b3e605874575acca813b1d123403c44c8e
Rakibul Islam <hridoy.9@gmail.com>
2021-08-11 16:41:13 +0600

    Game logic v1.3

--- javascript.js
+++ javascript.js
@@ -28,6 +28,40 @@ document.getElementById("startreset").onclick = function () {
     }
 };
 
+//Clicking on an answer box
+
+for(i=1; i<5; i++){
+    document.getElementById("box"+i).onclick = function(){
+        //check if we are playing     
+        if(playing == true){//yes
+            if(this.innerHTML == correctAnswer){
+            //correct answer
+                
+                //increase score by 1
+                score++;
+                document.getElementById("scorevalue").innerHTML = score;
+                //hide wrong box and show correct box
+                hide("wrong");
+                show("correct");
+                setTimeout(function(){
+                    hide("correct");   
+                }, 1000);
+                
+                //Generate new Q&A
+                
+                generateQA();
+            }else{
+            //wrong answer
+                hide("correct");
+                show("wrong");
+                setTimeout(function(){
+                    hide("wrong");   
+                }, 1000);
+            }
+        }
+    }
+}   
+
 // functions
 
 // start counter
@@ -64,10 +98,30 @@ function show(id) {
     document.getElementById(id).style.display = "block";
 }
 // generate multiple questions and answers
-function generateQA() {
-    
-}
 
+function generateQA(){
+    var x = 1+ Math.round(9*Math.random());
+    var y = 1+ Math.round(9*Math.random());
+    correctAnswer = x*y;
+    document.getElementById("question").innerHTML = x + "x" + y;
+    var correctPosition = 1+ Math.round(3*Math.random());
+    document.getElementById("box"+correctPosition).innerHTML = correctAnswer; //fill one box with the correct answer
+    
+    //fill other boxes with wrong answers
+    
+    var answers = [correctAnswer];
+    
+    for(i=1; i<5; i++){
+        if(i != correctPosition) {
+            var wrongAnswer;
+            do{
+                wrongAnswer = (1+ Math.round(9*Math.random()))*(1+ Math.round(9*Math.random())); //a wrong answer
+            }while(answers.indexOf(wrongAnswer)>-1)
+            document.getElementById("box"+i).innerHTML = wrongAnswer;
+            answers.push(wrongAnswer);
+        }
+    }
+}
 
 //if we click on the start/reset
     //if we are playing 
