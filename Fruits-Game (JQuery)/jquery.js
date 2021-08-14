eb9e6bfb1d1edac806b09d8532415d68a215ee08
Rakibul Islam <hridoy.9@gmail.com>
2021-08-14 20:44:09 +0600

    Fruits Game Logic v1.2

--- Fruits-Game (JQuery)/jquery.js
+++ Fruits-Game (JQuery)/jquery.js
@@ -1,6 +1,7 @@
 let playing = false;
 let score;
 let trialsLeft;
+let fruits = ['GreenApple', 'Lemon', 'Mango', 'Orange', 'Pineapple', 'Tomato'];
 $(function(){
     // click on start reset button
     $("#startreset").on("click", function() {
@@ -19,6 +20,10 @@ $(function(){
             $("#trialsLeft").show();
             trialsLeft = 3;
             addHearts();
+            // change button text to reset game
+            $("#startreset").html("Reset Game");
+            // start sending fruits
+            startAction();
         }
     });
 
@@ -29,4 +34,17 @@ function addHearts() {
     for (let i = 0; i < trialsLeft; i++) {
         $("#trialsLeft").append(" &#10084; ");
     }
+}
+
+function startAction() {
+    // var greenApple = '<img src="images/GreenApple.png" alt="apple" class="fruits">';
+    // $("#fruitsContainer").append(greenApple);
+    $("#fruit1").show();
+    chooseFruit(); // choose a randon fruit
+    $('#fruit1').css({'left': Math.round(550*Math.random()), 'top': -50});
+}
+
+// generate a random fruit
+function chooseFruit() {
+    $("#fruit1").attr('src', 'images/' + fruits[Math.round(5*Math.random())] + '.png')
 }
